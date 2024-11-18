30aec184fb04b1a7385a743fda6ebfe69c916380
Udayshankar Ravikumar <uralstech@gmail.com>
2024-11-18 22:41:53 +0530

    Fix dummy.cs

--- UCloudTextToSpeech/Packages/com.uralstech.ucloudtexttospeech/Runtime/Scripts/Dummy.cs
+++ UCloudTextToSpeech/Packages/com.uralstech.ucloudtexttospeech/Runtime/Scripts/Dummy.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
-/// <summary>
-/// This is a dummy class for testing docfx.
-/// </summary>
-public class Dummy
+namespace Uralstech.UCloudTextToSpeech
 {
-    
+    /// <summary>
+    /// This is a dummy class for testing docfx.
+    /// </summary>
+    public class Dummy
+    {
+
+    }
 }
